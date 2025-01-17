1977695614712621f9dece82a989bc6493b1d943
Mr Tech <koechfestus003@gmail.com>
2025-01-17 17:38:32 +0300

    Final agent

--- finance-agent/finance_agent.py
+++ finance-agent/finance_agent.py
@@ -4,16 +4,6 @@ from  dotenv import load_dotenv
 from phi.tools.yfinance import YFinanceTools
 load_dotenv()
 
-<<<<<<< HEAD
-agent = Agent(
-    model = Groq(id="llama-3.3-70b-versatile"),
-    tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True)],
-    show_tool_calls=True,
-    markdown=True,
-    instructions=['Use tables to display data.']
-)
-agent.print_response("Summarize and compare analyst recommendations and fundamentals for TSLA and Phidata. Show in tables.")
-=======
 def get_company_symbol(company: str) -> str:
     """Use this function to get the symbol for a company.
 
@@ -48,4 +38,3 @@ agent = Agent(
     
 )
 agent.print_response("Summarize and compare analyst recommendations and fundamentals for TSLA and Phidata. Show in tables.", stream=True)
->>>>>>> 3fdd215 (Final agent)
